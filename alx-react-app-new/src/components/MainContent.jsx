cd9c79aaa9a98817528a3cda26c6a42c59283dad
Ivan Kiweewa Mulinda <ivankm2000@gmail.com>
2025-09-07 22:06:36 +0200

    Add styles

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+function MainContent() {
+    return (
+    <main style={{ 
+        backgroundColor: '#f0f8ff', 
+        padding: '20px', 
+        textAlign: 'center',
+        margin: '10px 0' 
+    }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+    );
+}
+
+export default MainContent;
