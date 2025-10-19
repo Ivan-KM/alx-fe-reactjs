4f9b18d8ad1030856e9828dc457ea71a812fbf9a
Ivan Kiweewa Mulinda <ivankm2000@gmail.com>
2025-10-19 22:48:41 +0300

    feat: create ProtectedRoute component using useAuth hook for authentication

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,10 +1,12 @@
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 export default function ProtectedRoute({ children }) {
-    const isAuthenticated = localStorage.getItem("isLoggedIn") === "true";
+    const { isAuthenticated } = useAuth();
 
     if (!isAuthenticated) {
         return <Navigate to="/login" replace />;
     }
+
     return children;
-}
+    }   
